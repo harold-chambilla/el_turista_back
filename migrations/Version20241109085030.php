3945d60c14217689fed9f8cfc3d14db99a360bf4
harolditoc <harold.chambilla@usil.pe>
2024-11-09 09:22:43 -0500

    iu

--- migrations/Version20241109085030.php
+++ migrations/Version20241109085030.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241109072436 extends AbstractMigration
+final class Version20241109085030 extends AbstractMigration
 {
     public function getDescription(): string
     {
