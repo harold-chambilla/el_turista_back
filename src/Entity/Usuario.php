6336142fa56eb55dff186e520be2a8cdd242cf55
M4HR <huertasramosmanuelangel@gmail.com>
2024-11-09 02:08:39 -0500

    apis globales

--- src/Entity/Usuario.php
+++ src/Entity/Usuario.php
@@ -3,11 +3,6 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
-use ApiPlatform\Metadata\Get;
-use ApiPlatform\Metadata\Delete;
-use ApiPlatform\Metadata\GetCollection;
-use ApiPlatform\Metadata\Patch;
-use ApiPlatform\Metadata\Post;
 use App\Repository\UsuarioRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
